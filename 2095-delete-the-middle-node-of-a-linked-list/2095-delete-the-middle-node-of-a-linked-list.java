e0ea0fe2a197818f8019b4c113659c33a7e8b65a
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2026-07-11 13:13:33 +0530

    Time: 3 ms (99.96%), Space: 65.6 MB (99.85%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -1,36 +1,30 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        if(head.next==null) return null;
-        ListNode temp=head;
-        int len=0;
-       while(temp!=null){
-        temp=temp.next;
-        len++;
-       }
-       temp=head;
-       for(int i=0;i<len/2-1;i++){
-        temp=temp.next;
-       }
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
 
-       
-       
-       temp.next=temp.next.next;
-       
-       
-
-
-
-
+        }
+        // if (prev != null) {
+            prev.next = slow.next;
+        // }
         return head;
     }
 }
