49606c58b8e99f2afda765742d6acbf33ca35143
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2026-08-06 02:35:15 +0530

    Time: 0 ms (100%), Space: 42.8 MB (61.44%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,39 +1,35 @@
 class Solution {
-    int[] dp = new int[101];
-
-    private int solve(int[] nums, int i, int n) {
-        if (i > n)
-            return 0;
-
-        if (dp[i] != -1)
-            return dp[i];
-
-        // Rob current house and move to i + 2
-        int take = nums[i] + solve(nums, i + 2, n);
-
-        // Skip current house and move to i + 1
-        int skip = solve(nums, i + 1, n);
-
-        return dp[i] = Math.max(take, skip);
-    }
-
     public int rob(int[] nums) {
         int n = nums.length;
 
         if (n == 1)
             return nums[0];
 
-        if (n == 2)
-            return Math.max(nums[0], nums[1]);
+        int[] dp = new int[n + 1];
 
-        // Case 1: Consider houses from 0 to n-2
-        Arrays.fill(dp, -1);
-        int take0thHouse = solve(nums, 0, n - 2);
+        int result1;
+        int result2;
 
-        // Case 2: Consider houses from 1 to n-1
-        Arrays.fill(dp, -1);
-        int take1stHouse = solve(nums, 1, n - 1);
+        // Case 1: Rob houses from 0 to n-2 (skip last house)
+        dp[0] = 0;
+        for (int i = 1; i <= n - 1; i++) {
+            dp[i] = Math.max(dp[i - 1],
+                    nums[i - 1] + (i - 2 >= 0 ? dp[i - 2] : 0));
+        }
+        result1 = dp[n - 1];
 
-        return Math.max(take0thHouse, take1stHouse);
+        // Case 2: Rob houses from 1 to n-1 (skip first house)
+        dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1],
+                    nums[i - 1] + dp[i - 2]);
+        }
+        result2 = dp[n];
+
+        return Math.max(result1, result2);
     }
 }
