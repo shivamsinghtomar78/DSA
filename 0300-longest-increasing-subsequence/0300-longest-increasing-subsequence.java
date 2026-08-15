09185b688d8a1e7addfb50873780210239710763
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2026-08-15 20:16:41 +0530

    Time: 5 ms (88.38%), Space: 46.3 MB (34.69%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,40 @@
 class Solution {
-    public int lengthOfLIS(int[] arr) {
-        int n=arr.length;
-        int mx=1;
-        int [] dp=new int[n];
-        Arrays.fill(dp,1);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                 if(arr[j]<arr[i]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                mx=Math.max(mx,dp[i]);
-                 }
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        List<Integer> sorted = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            /*
+                Why lower bound?
+                We want an increasing subsequence, and hence
+                we want to eliminate the duplicates as well.
+                lower_bound returns the index of "next greater or equal to."
+            */
+            int index = binarySearch(sorted, nums[i]);
+
+            if (index == sorted.size())
+                sorted.add(nums[i]); // greatest: so insert it
+            else
+                sorted.set(index, nums[i]); // replace
+        }
+
+        return sorted.size();
+    }
+
+    private int binarySearch(List<Integer> sorted, int target) {
+        int left = 0, right = sorted.size();
+        int result = sorted.size();
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (sorted.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                result = mid;
+                right = mid;
             }
         }
-        return mx;
+        return result;
     }
 }
